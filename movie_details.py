ba612c163b59c3247295439b00574f053780fe9b
shiwammittal <shiwammittal@gmail.com>
2017-03-28 15:26:38 -0400

    Pep8 requirements fulfilled

--- movie_details.py
+++ movie_details.py
@@ -1,14 +1,16 @@
 import webbrowser
-#import webbrowser class to be able to open youtube urls
+
+
+# import webbrowser class to be able to open youtube urls
 class Movie():
-    #Initializing Movie class variables
-    def __init__(self, movie_title, movie_storyline, movie_poster_url, movie_trailer_url):
+    # Initializing Movie class variables
+    def __init__(self, movie_title, movie_storyline,
+                 movie_poster_url, movie_trailer_url):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster_url
         self.trailer_youtube_url = movie_trailer_url
 
-    #Play movie trailers
+    # Play movie trailers
     def run_youtube_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
