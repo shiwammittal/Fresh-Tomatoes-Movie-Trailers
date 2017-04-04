63c14be255b3e0a6407487479651f20eb3297227
shiwammittal <shiwammittal@gmail.com>
2017-04-04 18:19:17 -0400

    One argument per line

--- movie_details.py
+++ movie_details.py
@@ -4,8 +4,11 @@ import webbrowser
 # import webbrowser class to be able to open youtube urls
 class Movie():
     # Initializing Movie class variables
-    def __init__(self, movie_title, movie_storyline,
-                 movie_poster_url, movie_trailer_url):
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 movie_poster_url,
+                 movie_trailer_url):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster_url
